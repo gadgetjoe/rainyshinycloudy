e5332ff079c2acab7fa2546783a8031b9a287372
Joe Mahaffey <joe@mahaffey.us>
2016-12-31 12:36:40 -0500

    IOS 10 App: Rainy Shine - 91, 92, 93
    91 - Table view, delegate, datasource
    92 - OpenWeather API Registration
    93 - Data model and constants

---
+++ rainyshinycloudy/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  rainyshinycloudy
+//
+//  Created by Joe Mahaffey on 12/31/16.
+//  Copyright © 2016 Joe Mahaffey. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "2fa47cfcc4ed29fed550a23b306ea92f"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+
