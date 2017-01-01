8349b211cabdedd7b30217bae45f34d743d4f71c
Joe Mahaffey <joe@mahaffey.us>
2017-01-01 17:38:16 -0500

    98. iOS 10 App: Rainy Shine - IBOutlets & custom weather cell
    98. iOS 10 App: Rainy Shine - IBOutlets & custom weather cell

--- rainyshinycloudy/WeatherVC.swift
+++ rainyshinycloudy/WeatherVC.swift
@@ -52,8 +52,9 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                         let forecast = Forecast(weatherDict: obj)
                         self.forecasts.append(forecast)
                         print(obj)
-                        
                     }
+                    self.forecasts.remove(at: 0)
+                    self.tableView.reloadData()
                 }
             }
             completed()
@@ -71,16 +72,20 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 6
+        return forecasts.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
-        
-        return cell
-
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as? WeatherCell {
+            
+            let forecast = forecasts[indexPath.row]
+            cell.configureCell(forecast: forecast)
+            return cell
+        } else {
+            return WeatherCell()
+        }
     }
     
     func updateMainUI() {
